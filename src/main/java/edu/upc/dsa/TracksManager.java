94ea16cece96b749dc6128d7db42c98c58ee18c9
Izan Pérez Cosano <izanperez98@gmail.com>
2019-03-22 19:56:42 +0100

    façade

--- src/main/java/edu/upc/dsa/TracksManager.java
+++ src/main/java/edu/upc/dsa/TracksManager.java
@@ -1,5 +1,6 @@
 package edu.upc.dsa;
 
+import edu.upc.dsa.models.Album;
 import edu.upc.dsa.models.Track;
 
 import java.util.List;
@@ -13,6 +14,10 @@ public interface TracksManager {
     public List<Track> findAll();
     public void deleteTrack(String id);
     public Track updateTrack(Track t);
+
+    public Album addAlbum (String id, String name, String singer, int year);
+    public Track addTrackToAlbum(String idTrack, String idAlbum);
+
 
     public int size();
 }
