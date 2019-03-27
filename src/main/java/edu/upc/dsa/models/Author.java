a1ad77a7e1c022bb7eeb4a1348b7928b79e6dd2a
Izan Pérez Cosano <izanperez98@gmail.com>
2019-03-27 21:38:21 +0100

    small corrections, replaced LinkedList and new add methods

--- src/main/java/edu/upc/dsa/models/Author.java
+++ src/main/java/edu/upc/dsa/models/Author.java
@@ -1,21 +1,35 @@
 package edu.upc.dsa.models;
 
-import java.util.List;
+import java.util.LinkedList;
 
 public class Author {
-    String name;
-    List<Track> tracks;
-    List<Album> albums;
 
+    //Attributes
+    private String name;
+    private LinkedList<Track> tracks;
+    private LinkedList<Album> albums;
+
+    //Constructors
     public Author() {
+
     }
 
-    public Author(String name, List<Track> tracks, List<Album> albums) {
+    public Author(String name) {
         this.name = name;
-        this.tracks = tracks;
-        this.albums = albums;
+        this.tracks = new LinkedList<>();
+        this.albums = new LinkedList<>();
     }
 
+    //Methods
+    public void addTrack(Track track) {
+        this.tracks.add(track);
+    }
+
+    public void addAlbum(Album album) {
+        this.albums.add(album);
+    }
+
+    //Getters and Setters
     public String getName() {
         return name;
     }
@@ -24,19 +38,19 @@ public class Author {
         this.name = name;
     }
 
-    public List<Track> getTracks() {
+    public LinkedList<Track> getTracks() {
         return tracks;
     }
 
-    public void setTracks(List<Track> tracks) {
+    public void setTracks(LinkedList<Track> tracks) {
         this.tracks = tracks;
     }
 
-    public List<Album> getAlbums() {
+    public LinkedList<Album> getAlbums() {
         return albums;
     }
 
-    public void setAlbums(List<Album> albums) {
+    public void setAlbums(LinkedList<Album> albums) {
         this.albums = albums;
     }
 }
